80244c49de7277765b03095529c03d4644a3a298
komeitmc <41131144+komeitmc@users.noreply.github.com>
2023-07-30 22:45:14 +0900

    Add files via upload

---
+++ dig18.js
@@ -0,0 +1,17 @@
+// ボタンとなるエレメント取得
+const btnElement = document.getElementById("btnBox");
+//表示するエレメント
+const dispElements=[];
+dispElements.push(document.getElementById("nextactionImg"));
+dispElements.push(document.getElementById("nextactionStr"));
+
+//イベント設定
+btnElement.addEventListener('click',dispNext);
+
+//表示
+function dispNext() {
+  console.log("visible",dispElements);
+  for(let i=0 ; i<dispElements.length ; i++) {
+    dispElements[i].style.visibility = "visible";
+  }
+}
